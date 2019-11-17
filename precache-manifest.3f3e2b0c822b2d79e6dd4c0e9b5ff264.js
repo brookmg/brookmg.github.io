8ff06f88529f10348df22b9463088511b921e9d2
brookmezgu <brookmezgebu64@gmail.com>
2019-11-17 16:07:05 +0300

    progress: first static build

---
+++ precache-manifest.3f3e2b0c822b2d79e6dd4c0e9b5ff264.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267498b2506eba2c6bbbef4db2143ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16115862293083cef5e",
+    "url": "/static/css/main.2d5f17a2.chunk.css"
+  },
+  {
+    "revision": "bea2e7331ddc3bbf1eff",
+    "url": "/static/js/2.e9c28860.chunk.js"
+  },
+  {
+    "revision": "f16115862293083cef5e",
+    "url": "/static/js/main.c8d3c92b.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "f023c2e6a2f1898c33e7ca034c9f5dc9",
+    "url": "/static/media/Azemari.f023c2e6.png"
+  },
+  {
+    "revision": "b3d2c32c113806af006d3494e28e67c3",
+    "url": "/static/media/Flick.b3d2c32c.png"
+  },
+  {
+    "revision": "cbb96b63dc4d960e4e562b80b71aca88",
+    "url": "/static/media/Hulu.cbb96b63.png"
+  },
+  {
+    "revision": "8d4c4972af4e8778eabf71475d48ef4b",
+    "url": "/static/media/Kuwas.8d4c4972.png"
+  },
+  {
+    "revision": "4e0a071c850c7091fff3ba41400d0a92",
+    "url": "/static/media/icon.4e0a071c.jpg"
+  }
+]);
